19c76706db4e76b929ed397bace5645f95379ef4
Radek <radek.halfaf@gmail.com>
2020-03-28 09:53:27 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class Main{
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        String 
+
+    }
+}
