2a3837ab115cfccd6fd733bdca0737ae87b4f10b
Radek <radek.halfaf@gmail.com>
2020-03-28 12:35:57 +0100

    Third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,29 @@
-import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main{
     public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-        String 
+
+
+
+        String url = "jdbc:mysql://localhost:3306/shop?serverTimezone=CET";
+        String user = "root";
+        String password = "Nirvana88";
+        String query = "SELECT*FROM country;";
+        try(Connection connection = DriverManager.getConnection(url,user,password);
+            Statement statement = connection.createStatement()) {
+
+
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt("CO_ID"));
+                System.out.println(resultSet.getString("CO_NAME"));
+                System.out.println(resultSet.getString("CO_ALIAS"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
