0926ad8afafc0e151e96bfbcd6a6f502005f97d0
Radek <radek.halfaf@gmail.com>
2020-03-28 13:14:44 +0100

    Zadanie 2

--- src/main/java/Zadanie2.java
+++ src/main/java/Zadanie2.java
@@ -1,5 +1,32 @@
+import util.DatabaseConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Zadanie2{
     public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("?");
+        String questionMark = ",?";
+        int[] tablica = new int[]{1, 3, 5};
 
+        for (int i = 1; i < tablica.length; i++) {
+            stringBuilder.append(questionMark);
+
+        }
+
+        String query = "update product set PRO_DESCRIPTION " +
+                "= CONCAT(PRO_DESCRIPTION, " +
+                "?) where pro_id in (" + stringBuilder.toString() + ")";
+
+        try (PreparedStatement preparedStatement = DatabaseConnection.getInstance().getConnection().prepareStatement(query)) {
+        preparedStatement.setString(1, " - BRAK");
+        for (int i = 0; i < tablica.length; i++){
+            preparedStatement.setInt(i+2,tablica[i]);
+        }
+        int count = preparedStatement.executeUpdate();
+            System.out.println("Zaktualizowano " + count + " rekordów");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
